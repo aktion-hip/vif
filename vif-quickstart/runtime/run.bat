5310bf06a20a05c362b6f2614017eb3c7a89b628
aktion-hip <benno.luthiger@id.ethz.ch>
2015-06-01 00:05:03 +0200

    Remove incorrect checkin

--- vif-quickstart/runtime/run.bat
+++
@@ -1,1 +0,0 @@
-java -Dlogback.configurationFile=./logback.xml -jar org.eclipse.osgi_3.9.1.v20140110-1610.jar -console
--- vif-quickstart/runtime/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# VIF Quickstart
-# (c) Benno Luthiger, Aktion HIP
-
-java -Dlogback.configurationFile=./logback.xml -jar org.eclipse.osgi_3.9.1.v20140110-1610.jar -console
